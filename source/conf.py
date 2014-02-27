015c360eced16275a446f775586cb6aeeed45c04
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-02-27 08:38:11 -0600

    updates based on Ryan Roemer's chevron nav demo

--- source/conf.py
+++ source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Scala Workshop at SIGCSE  documentation build configuration file, created by
-# sphinx-quickstart on Sun Jan  6 10:30:22 .
+# Introduction to Computer Science in C# documentation build configuration file, created by
+# sphinx-quickstart on Tue Dec  6 14:14:02 2011.
 #
 # This file is execfile()d with the current directory set to its containing dir.
 #
@@ -13,7 +13,6 @@
 
 import sys, os
 import sphinx_bootstrap_theme
-
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -29,6 +28,8 @@ import sphinx_bootstrap_theme
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.todo', 'sphinx.ext.mathjax']
 
+todo_include_todos = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -42,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Scala Workshop at SIGCSE'
-copyright = u', Lewis, Läufer, and Thiruvathukal'
+project = u'SIGCSE Scala Workshop'
+copyright = u'2012-2014, Konstantin Läufer, Mark Lewis and George Thiruvathukal'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -62,11 +63,11 @@ release = '1.0'
 # non-false value, then it is used:
 #today = ''
 # Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
+today_fmt = '%d-%B-%Y %H:%M:%S'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['_build','stringchar.rst', 'gcdfp.rst']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
@@ -88,6 +89,43 @@ pygments_style = 'sphinx'
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
+rst_prolog = """
+
+.. |bitbucket| image:: figures/bitbucket.png
+             :alt: Bitbucket
+             :align: middle
+             
+.. |instructions| image:: figures/instructions.png
+             :alt: Instructions
+             :align: middle
+
+.. |pdf| image:: figures/pdf.png
+             :alt: PDF
+             :align: middle
+
+.. |epub| image:: figures/epub.png
+             :alt: ePub
+             :align: middle
+
+.. |html| image:: figures/html.png
+             :alt: HTML
+             :align: middle
+
+
+.. highlight:: scala
+   
+"""
+
+rst_epilog = """
+.. |if-else| replace:: ``if``\ -``else``
+
+.. |if-else-if| replace:: ``if``\ -``else``\ -``if``
+
+.. |do-while| replace:: ``do``\ -``while``
+
+"""
+
+
 
 # -- Options for HTML output ---------------------------------------------------
 
@@ -98,7 +136,6 @@ html_theme = 'bootstrap'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
 html_theme_options = {
     # Navigation bar title. (Default: ``project`` value)
     'navbar_title': "SIGCSE Scala",
@@ -110,6 +147,8 @@ html_theme_options = {
     # A list of tuples containting pages to link to.  The value should
     # be in the form [(name, page), ..]
     'navbar_links': [('Index', 'genindex')],
+
+    'navbar_sidebarrel': True,
 
     # Include hidden TOCs in Site navbar?
     #
@@ -127,7 +166,7 @@ html_theme_options = {
 
     # Fix navigation bar to top of page?
     # Values: "true" (default) or "false"
-    'navbar_fixed_top': "true",
+    #'navbar_fixed_top': "true",
 
     # Location of link to source.
     # Options are "nav" (default), "footer" or anything else to exclude.
@@ -139,20 +178,21 @@ html_theme_options = {
     # such as "amelia" or "cosmo".
     #
     # Note that this is served off CDN, so won't be available offline.
-    'bootswatch_theme': "united",
+    'bootswatch_theme': "lumen",
 
-    'bootstrap_version' : "2"
+    'bootstrap_version' : "3"
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+#html_short_title = 'C# Intro'
+html_short_title = 'sigcse-scala'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -166,12 +206,12 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = []
+
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
-
+html_last_updated_fmt = '%d-%B-%Y %H:%M:%S'
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
 #html_use_smartypants = True
@@ -196,7 +236,7 @@ html_static_path = ['_static']
 #html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #html_show_copyright = True
@@ -210,8 +250,9 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ScalaWorkshopatSIGCSEdoc'
+htmlhelp_basename = 'sigcse-scala'
 
+highlight_language = 'csharp'
 
 # -- Options for LaTeX output --------------------------------------------------
 
@@ -229,7 +270,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'ScalaWorkshopatSIGCSE.tex', u'Scala Workshop at SIGCSE  Documentation',
+  ('index', 'sigcse-scala.tex', u'SIGCSE Workshop on Scala',
    u'Lewis, Läufer, and Thiruvathukal', 'manual'),
 ]
 
@@ -259,8 +300,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'scalaworkshopatsigcse', u'Scala Workshop at SIGCSE  Documentation',
-     [u'Lewis, Läufer, and Thiruvathukal'], 1)
+    ('index', 'introcscsharp', u'SIGCSE Workshop',
+     [u'Mark Lewis, Konstantin Läufer, and George K. Thiruvathukal'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -273,8 +314,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ScalaWorkshopatSIGCSE', u'Scala Workshop at SIGCSE  Documentation',
-   u'Lewis, Läufer, and Thiruvathukal', 'ScalaWorkshopatSIGCSE', 'One line description of project.',
+  ('index', 'sigcse-scala', u'SIGCSE Workshop on Scala',
+   u'Lewis, Läufer, and Thiruvathukal', 'sigcse-scala', 
+   'SIGCSE Scala',
    'Miscellaneous'),
 ]
 
@@ -288,44 +330,8 @@ texinfo_documents = [
 #texinfo_show_urls = 'footnote'
 
 
-# -- Options for Epub output ---------------------------------------------------
 
-# Bibliographic Dublin Core info.
-epub_title = u'Scala Workshop at SIGCSE '
-epub_author = u'Lewis, Läufer, and Thiruvathukal'
-epub_publisher = u'Lewis, Läufer, and Thiruvathukal'
-epub_copyright = u', Lewis, Läufer, and Thiruvathukal'
+# -- Options for epub output"
 
-# The language of the text. It defaults to the language option
-# or en if the language is not set.
-#epub_language = ''
+epub_basename = 'sigcse-scala'
 
-# The scheme of the identifier. Typical schemes are ISBN or URL.
-#epub_scheme = ''
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#epub_identifier = ''
-
-# A unique identification for the text.
-#epub_uid = ''
-
-# A tuple containing the cover image and cover page html template filenames.
-#epub_cover = ()
-
-# HTML files that should be inserted before the pages created by sphinx.
-# The format is a list of tuples containing the path and title.
-#epub_pre_files = []
-
-# HTML files shat should be inserted after the pages created by sphinx.
-# The format is a list of tuples containing the path and title.
-#epub_post_files = []
-
-# A list of files that should not be packed into the epub file.
-#epub_exclude_files = []
-
-# The depth of the table of contents in toc.ncx.
-#epub_tocdepth = 3
-
-# Allow duplicate toc entries.
-#epub_tocdup = True
